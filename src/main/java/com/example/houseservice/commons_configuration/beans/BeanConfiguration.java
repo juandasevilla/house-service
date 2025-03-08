34bd42a7a9f1f0a5bc89b42f84c98b04a7cf9bc2
Juan David Sevilla <sevilla.juan@correounivalle.edu.co>
2025-03-08 01:28:29 -0500

    feat(): add services, dto and mapper in application level

---
+++ src/main/java/com/example/houseservice/commons_configuration/beans/BeanConfiguration.java
@@ -0,0 +1,13 @@
+package com.example.houseservice.commons_configuration.beans;
+
+import com.example.houseservice.domain.ports.in.CategoryServicePort;
+import com.example.houseservice.domain.usecases.CategoryUseCase;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class BeanConfiguration {
+
+}
